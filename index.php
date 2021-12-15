7e2953760120b960b1c27654da71c31f6d9317ac
bobbykim89 <bobby.sihun.kim@gmail.com>
2021-12-15 00:05:43 -0700

    added pagination and custom single post page

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
       <div class="post-item">
         <h2 class='headline headline--medium headline--post-title'><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
         <div class="metabox">
-          <p>Posted by <?php the_author_posts_link() ?> on <?php the_time('n.j.y') ?> in <?php echo get_the_category_list(', ') ?> </p>
+          <p>Posted by <?php the_author_posts_link() ?> on <?php the_time('n.j.y') ?> in <?php echo get_the_category_list(', ') ?></p>
         </div>
         <div class="generic-content">
           <?php the_excerpt() ?>
@@ -25,6 +25,7 @@
         </div>
       </div>
     <?php }
+    echo paginate_links();
   ?>
 </div>
 
